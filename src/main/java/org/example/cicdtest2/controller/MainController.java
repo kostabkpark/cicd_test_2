dfc2d54d30fbaa7cbf88dd747ec22a87bb2e661b
kostabkpark <kostabkpark@gmail.com>
2025-06-23 16:32:09 +0900

    cicd auto

---
+++ src/main/java/org/example/cicdtest2/controller/MainController.java
@@ -0,0 +1,12 @@
+package org.example.cicdtest2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+  @GetMapping
+  public String hello() {
+    return "This is cicd test 2 --- auto deployment with github actions";
+  }
+}
