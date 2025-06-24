957a81fb50d09b145f6aa8b1f61c4cc5e107ca59
kostabkpark <kostabkpark@gmail.com>
2025-06-24 10:16:50 +0900

    autodeployment add

---
+++ src/test/java/org/example/cicdtest2/controller/MainControllerTest.java
@@ -0,0 +1,15 @@
+package org.example.cicdtest2.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MainControllerTest {
+
+  @Test
+  void hello() {
+    throw new RuntimeException("test failed");
+  }
+}
